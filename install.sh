f1d79de455fd16bf62670f02e48b710d60eff77d
Daniel Nunes <daniel.henri.nunes@gmail.com>
2018-04-03 15:01:52 +0100

    Rename config files in repo

--- install.sh
+++ install.sh
@@ -2,37 +2,25 @@
 
 BASEDIR="$(dirname "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")")"
 OLDDIR="$HOME/$(basename "$BASEDIR")"_old
-DOTFILES="$BASEDIR/.*"
-IGNOREFILES=". .. .git .gitignore .gitattributes .gitmodules"
+DOTFILES="gitconfig mail mailcap mbsyncrc msmtprc mutt taskrc vim vimrc"
 
 echo "Updating submodules..."
 (cd "$BASEDIR" && git submodule init && git submodule update --remote)
 
 for file in $DOTFILES; do
-  filetoignore=0
-  for filename in $IGNOREFILES; do
-    if [ "$filename" = "$(basename "$file")" ]; then
-      filetoignore=1
-      break
-    fi
-  done
-  if [ "$filetoignore" -eq 1 ]; then
-    continue
-  fi
-
-  if [ -e "$HOME/$(basename "$file")" ]; then
-    if [ -L "$HOME/$(basename "$file")" ]; then
-      echo "Removing $HOME/$(basename "$file")"
-      rm "$HOME/$(basename "$file")"
+  if [ -e "$HOME/.$file" ]; then
+    if [ -L "$HOME/.$file" ]; then
+      echo "Removing $HOME/.$file"
+      rm "$HOME/.$file"
     else
-      echo "Moving ""$HOME"/"$(basename "$file")"" to ""$OLDDIR"/"$(basename "$file")"""
+      echo "Moving ""$HOME"/."$file"" to ""$OLDDIR"/."$file"""
       mkdir -p "$OLDDIR"
-      mv "$HOME/$(basename "$file")" "$OLDDIR/$(basename "$file")"
+      mv "$HOME/.$file" "$OLDDIR/.$file"
     fi
   fi
 
-  echo "Creating link at ""$HOME"/"$(basename "$file")"""
-  ln -s "$file" "$HOME/$(basename "$file")"
+  echo "Creating link at ""$HOME"/."$file"""
+  ln -s "$BASEDIR/$file" "$HOME/.$file"
 done
 
 
